73f69943ffd151a7f4f77c831f9e4289d254cd06
Luis Romero <lueduar15@gmail.com>
2024-03-16 00:27:17 -0400

    database two

--- src/config/batabase/database.js
+++ src/config/batabase/database.js
@@ -1,6 +1,34 @@
-const { sequelize, Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize')
 const { envs } = require('../enviroments/enviroments')
 
 const sequelize = new Sequelize(envs.DB_URI, {
     logging: false, //false evita ruido en la consola
 })
+
+
+//Autenticacion de base de datos
+const authenticated = async() => {
+    try {
+        await sequelize.authenticate();
+        console.log('Conection established successfully...!');
+    } catch (error) {
+        console(error);
+    }
+}
+
+//Sincronizacion de base de datos
+const syncUp = async() => {
+    try {
+        await sequelize.sync();
+        console.log('Conectionn synced successfully...!');
+    } catch (error) {
+        console(error);
+    }
+}
+
+
+module.exports = {
+    authenticated,
+    syncUp,
+   Sequelize
+}
