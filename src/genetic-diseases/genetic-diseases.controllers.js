f95b83a6898df23ba5527aafb992cb9cb5aa3657
Luis Romero <lueduar15@gmail.com>
2024-06-18 00:13:59 -0400

    update controllers

--- src/genetic-diseases/genetic-diseases.controllers.js
+++ src/genetic-diseases/genetic-diseases.controllers.js
@@ -24,44 +24,60 @@ const findAll = async (req,res) => {
 
 
 const create = async(req,res) => {
-    const { requestTime} = req;
-    const { name, description, cause, mortalityRate, treatment, symptoms } = req.body;
-    // 1 const disease = req.body
-    const geneticDiseases = await GeneticDiseasesServices.create({
-        name,
-        description,
-        cause,
-        mortalityRate,
-        treatment,
-        symptoms,
-    })
-    return res.status(201).json({
-        // 1 data: disease,
-        requestTime,
-        data: geneticDiseases,
+    try {
+        const { requestTime} = req;
+        const { name, description, cause, mortalityRate, treatment, symptoms } = req.body;
+        // 1 const disease = req.body
+        const geneticDiseases = await GeneticDiseasesServices.create({
+            name,
+            description,
+            cause,
+            mortalityRate,
+            treatment,
+            symptoms,
+        })
+        return res.status(201).json({
+            // 1 data: disease,
+            requestTime,
+            data: geneticDiseases,
 
-    })
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error
+        })
+    }
 }
 
 
 const findOne = async(req,res) => {
-    const { requestTime } = req;
-    const { id } = req.params;
-    const geneticDisease = await GeneticDiseasesServices.findOne(id)
+    try {
+        const { requestTime } = req;
+        const { id } = req.params;
+        const geneticDisease = await GeneticDiseasesServices.findOne(id)
 
-    if(!geneticDisease){
-        return res.status(404).json({
-            status: "error",
-            message: `Genetic Disease with ${id} not found`,
+        if(!geneticDisease){
+            return res.status(404).json({
+                status: "error",
+                message: `Genetic Disease with ${id} not found`,
+            })
+        }
+
+        return res.status(200).json({
+            message: "method get - findOne",
+            id: req.params.id, //muestra en id en el front
+            requestTime,
+            geneticDisease
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error
         })
     }
-
-    return res.status(200).json({
-        message: "method get - findOne",
-        id: req.params.id, //muestra en id en el front
-        requestTime,
-        geneticDisease
-    })
 }
 
 
@@ -109,27 +125,37 @@ const update = async(req,res) => {
 
 
 const toDelete = async(req,res)=>{
-    //const { requestTime, saludo2 } = req;
-    const {id} = req.params;
+    try {
+        //const { requestTime, saludo2 } = req;
+        const {id} = req.params;
 
-    const geneticDisease = await GeneticDiseasesServices.findOne(id)
-
-    if(!geneticDisease){
-        return res.status(400).json({
-            status: "error",
-            message: `Genetic Disease with ${id} not found`,
+        const geneticDisease = await GeneticDiseasesServices.findOne(id)
+    
+        if(!geneticDisease){
+            return res.status(400).json({
+                status: "error",
+                message: `Genetic Disease with ${id} not found`,
+            })
+        }
+    
+        await GeneticDiseasesServices.delete(geneticDisease)
+    
+        return res.status(200).json({
+            message:"method delete - remove",
+            //requestTime,
+            //id:id,
+            //saludo2
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error
         })
     }
-
-    await GeneticDiseasesServices.delete(geneticDisease)
-
-    return res.status(200).json({
-        message:"method delete - remove",
-        //requestTime,
-        //id:id,
-        //saludo2
-    })
  }
+
+
 
  module.exports = {
     findAll,
